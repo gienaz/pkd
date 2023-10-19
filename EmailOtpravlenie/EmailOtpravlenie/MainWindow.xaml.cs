7d8d53bbfcaa101d6ed747afe9714ecf65a45bec
gienaz <stspthv13@gmail.com>
2023-10-19 14:16:12 +0700

    wpf2

---
+++ EmailOtpravlenie/EmailOtpravlenie/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmailOtpravlenie
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //отправитель
+        //адрес отправителя
+        //получатель
+        //адрес получателя
+        //тип посылки(письмо, бандероль, посылка)
+        //хрупкая посылка(да\нет)
+        //отправление за границу(да\нет)
+
+
+        int cost = 0;
+        int letterCost = 100;
+        int banderCost = 200;
+        int boxCost = 500;
+        bool toFarbool = false;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ComboBox.Text == "Письмо")
+            {
+                cost = letterCost;
+            }
+            else if(ComboBox.Text == "Бандероль")
+            {
+                cost = banderCost;
+            }
+            else if(ComboBox.Text == "Посылка")
+            {
+                cost = boxCost;
+            }
+
+            Recalculate();
+        }
+
+        void Recalculate()
+        {
+            if (toFarbool == true) cost += 300;
+            Cost.Content= "Итоговая цена: " + cost;
+        }
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void toFar_Checked(object sender, RoutedEventArgs e)
+        { 
+            if(toFarbool == true)
+            {
+                toFarbool = false;
+            }
+            else
+            {
+                toFarbool = true;
+            }
+            Recalculate();
+        }
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,162 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        int nub;
+        int nubs;
+        int outp;
+        int result;
+
+
+        private void BtnText_Click(object sender, EventArgs e)
+        {
+
+            TB1.Text = TB1.Text + "1";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "3";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + " + ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "2";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "6";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "7";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "9";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "8";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            TB1.Text = "";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + "0";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            double result = 0;
+
+            string operation = "";
+
+            string[] tokens = TB1.Text.Split(' ');
+
+            if (tokens.Length % 2 == 1)
+            {
+                for (int i = 0; i < tokens.Length; i += 2)
+                {
+                    if (double.TryParse(tokens[i], out double value))
+                    {
+                        if (i == 0)
+                        {
+                            result = value;
+                        }
+                        else
+                        {
+                            switch (operation)
+                            {
+                                case "+":
+                                    result += value;
+                                    break;
+                                case "-":
+                                    result -= value;
+                                    break;
+                                case "*":
+                                    result *= value;
+                                    break;
+                                case "/":
+                                    result /= value;
+                                    break;
+                                default:
+                                    return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+                    if (i + 1 < tokens.Length)
+                    {
+                        operation = tokens[i + 1];
+                    }
+                }
+
+                TB1.Text = "" + result;
+            }
+        }
+
+        private void btnminus_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + " - ";
+        }
+
+        private void btnumn_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + " * ";
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            TB1.Text = TB1.Text + " / ";
+        }
+
+        private void TB1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) { }
+        private void Button_Click_2(object sender, RoutedEventArgs e) { }
+
+
+
     }
 }
