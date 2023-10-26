f63bfaa7940f0b707db4d56278ed7f1a49f30b0b
gienaz <stspthv13@gmail.com>
2023-10-26 14:18:54 +0700

    asda

---
+++ Planirovshik/Planirovshik/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Planirovshik
+{
+
+    public partial class MainWindow : Window
+    {
+        string savePath = "C:\\Users\\User\\Desktop\\TaskHolder";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task testTask = new Task();
+
+        }
+        private void NewTask_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new Window2();
+            if (w.ShowDialog() == true)
+            {
+                Task task = w.Dialogue;
+            }
+        }
+
+        void AddTaskToList(Task task)
+        {
+
+        }
+    }
+
+
+    public class Task
+    {
+        public string name {  get; set; }
+        public int priority { get; set; }
+            public string deadline { get; set; }// = new DateTime(2008, 3, 1, 7, 0, 0);  Displays 01/03/2008 07:00:00
+            public string description { get; set; }
+            public bool isCompleted { get; set; }
+
+            public Task newTask(string name, int priority, DateTime deadline, string description, bool isCompleted)
+        {
+            Task task = new Task();
+            task.name = name;
+            task.priority = priority;
+            task.deadline = deadline;
+            task.description = description;
+            task.isCompleted = isCompleted;
+            return task;
+        }
+    }
+}
---
+++ Planirovshik/Planirovshik/Window2.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Planirovshik
+{
+    public partial class Window2 : Window
+    {
+        Task task;
+        public Window2()
+        {
+            InitializeComponent();
+            ComboBox_Priority.SelectedIndex = 2;
+            Date.SelectedDate = DateTime.Now;
+            task = new Task();
+        }
+
+        public Task Dialogue
+        {
+            get { return task; }
+        }
+
+        bool CheckIfCan()
+        {
+            bool temp = false;
+            if(Add_Name.Text.Length > 0 && Add_Desc.Text.Length > 0)
+
+            return temp;
+        }
+
+        private void Add_Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            task.name = Add_Name.Text;
+        }
+        private void ComboBox_Priority_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ////////////////
+        }
+        private void Date_Changed(object sender, SelectedCellsChangedEventArgs e)
+        {
+            task.deadline = Date.Text;
+        }
+        private void Add_Desc_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            task.description = Add_Desc.Text;
+        }
+
+        private void btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
