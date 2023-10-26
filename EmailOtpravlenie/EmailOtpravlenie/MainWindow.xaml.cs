2a11326b71297133cad1fed4249437b0ec85bc5d
gienaz <stspthv13@gmail.com>
2023-10-26 10:10:33 +0700

    form sender done

--- EmailOtpravlenie/EmailOtpravlenie/MainWindow.xaml.cs
+++ EmailOtpravlenie/EmailOtpravlenie/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -34,67 +36,122 @@ namespace EmailOtpravlenie
         int banderCost = 200;
         int boxCost = 500;
         bool toFarbool = false;
+        bool isreadytouse = false;
 
 
         public MainWindow()
         {
             InitializeComponent();
+            ComboBox.SelectedIndex = 0;
+            Button.IsEnabled = false;
+            TextBox4.Text = "mofeyax639@scubalm.com";
+
         }
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            CheckIfYouCan();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(ComboBox.Text == "Письмо")
+            if(ComboBox.SelectedIndex == 1)
             {
                 cost = letterCost;
             }
-            else if(ComboBox.Text == "Бандероль")
+            else if(ComboBox.SelectedIndex == 2)
             {
                 cost = banderCost;
             }
-            else if(ComboBox.Text == "Посылка")
+            else if(ComboBox.SelectedIndex == 3)
             {
                 cost = boxCost;
             }
-
+            CheckIfYouCan();
             Recalculate();
         }
-
-        void Recalculate()
+         void CheckIfYouCan()
+        {
+            if (TextBox.Text.Length > 0 && TextBox4.Text.Length > 0 && TextBox1.Text.Length > 0 && TextBox2.Text.Length > 0 && TextBox3.Text.Length > 0 && ComboBox.SelectedIndex != 0)
+            {
+                isreadytouse = true;
+                Button.IsEnabled = true;
+            }
+            else
+            {
+                isreadytouse = true;
+                Button.IsEnabled = false;
+            }
+        }
+        void Recalculate(bool boolean=true)
         {
             if (toFarbool == true) cost += 300;
+            else if(boolean == false) cost -= 300;
             Cost.Content= "Итоговая цена: " + cost;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isreadytouse)
+            {
+                string[] mystrings = new string[] { "Отправитель: " + TextBox.Text, "Адрес отправления: " + TextBox1.Text, "Получатель: " + TextBox2.Text,
+                                                "Адрес получателя: " + TextBox3.Text,
+                                                "Вид отправления: " + ComboBox.Text, "За границу: " + toFar.Content, "Итоговая стоимость: " + cost};
+
+                System.IO.File.WriteAllLines($"C://Users/User/Desktop/pismo.txt", mystrings);
+
+                Send();
+                Environment.Exit(0);
+            }
+
+        }
+        void Send()
+        {
+            string path = "C://Users/User/Desktop/pismo.txt";
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("npl1u1pc@mail.ru", "Stass");
+            // кому отправляем
+            MailAddress to = new MailAddress(TextBox4.Text);
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = "OpisaniePosilki";
+            m.Attachments.Add(new Attachment(path));
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.mail.ru", 2525);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("npl1u1pc@mail.ru", "dWxZks4tnrEGDq3XWqQh");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            //npl1u1pc@mail.ru
         }
 
         private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            CheckIfYouCan();
         }
 
         private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            CheckIfYouCan();
         }
 
         private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            CheckIfYouCan();
         }
 
         private void toFar_Checked(object sender, RoutedEventArgs e)
         { 
-            if(toFarbool == true)
-            {
-                toFarbool = false;
-            }
-            else
-            {
-                toFarbool = true;
-            }
+            toFarbool = true;
             Recalculate();
+        }
+        private void toFar_Unchecked(object sender, RoutedEventArgs e)
+        { 
+            toFarbool = false;
+            Recalculate(false);
+        }
+
+        private void TextBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfYouCan();
         }
     }
 }
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public partial class SendForm : Form
+        {
+            // Типы отправления
+            enum SendType
+            {
+                Package, // Посылка
+                Letter, // Письмо
+                Parcel // Бандероль
+            }
+
+            // Типы доставки
+            enum DeliveryType
+            {
+                Domestic, // По стране
+                International // За границу
+            }
+
+            // Стоимость отправления в рублях за килограмм в зависимости от типа отправления и доставки
+            double[,] sendCost = new double[3, 2] { { 200, 500 }, { 100, 300 }, { 150, 400 } };
+            private void sendButton_Click(object sender, EventArgs e)
+            {
+                // Получаем данные из полей ввода
+                string senderName = senderNameTextBox.Text;
+                string senderAddress = senderAddressTextBox.Text;
+                string receiverName = receiverNameTextBox.Text;
+                string receiverAddress = receiverAddressTextBox.Text;
+                double weight = (double)weightNumericUpDown.Value;
+                SendType sendType = (SendType)sendTypeComboBox.SelectedIndex;
+                DeliveryType deliveryType = (DeliveryType)deliveryTypeComboBox.SelectedIndex;
+                string email = emailTextBox.Text;
+
+                // Проверяем корректность данных
+                if (string.IsNullOrEmpty(senderName) || string.IsNullOrEmpty(senderAddress) || string.IsNullOrEmpty(receiverName) || string.IsNullOrEmpty(receiverAddress) || weight <= 0 || sendType == null || deliveryType == null || string.IsNullOrEmpty(email))
+                {
+                    MessageBox.Show("Пожалуйста, заполните все поля корректно.");
+                    return;
+                }
+
+                // Рассчитываем стоимость отправления
+                double cost = sendCost[(int)sendType, (int)deliveryType] * weight;
+
+                // Формируем отчет об отправлении
+                string report = $"Отправитель: {senderName}\nАдрес отправителя: {senderAddress}\nПолучатель: {receiverName}\nАдрес получателя: {receiverAddress}\nТип отправления: {sendType}\nТип доставки: {deliveryType}\nВес: {weight} кг\nСтоимость: {cost} руб.";
+
+                // Отправляем отчет на email
+                try
+                {
+                    MailMessage message = new MailMessage("sendwindow@example.com", email, "Отчет об отправлении", report);
+                    SmtpClient client = new SmtpClient("smtp.example.com");
+                    client.Send(message);
+                    MessageBox.Show("Отчет успешно отправлен на email.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка при отправке отчета на email: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
