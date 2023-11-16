a82d16d795183feea2ba8fd834063eb72ffc4258
gienaz <stspthv13@gmail.com>
2023-11-16 11:39:17 +0700

    a

---
+++ WPFcontrosh/WPFcontrosh/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFcontrosh
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AsyncExample();
+        }
+        private async void AsyncExample()
+        {
+            while (true)
+            {
+                int sum = 0;
+                for (int i = 0; i < 200; i++) 
+                {
+                    await Task.Run(() =>
+                    {
+                        sum += i;
+                    });
+                    message.Text += sum.ToString();
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Click");
+
+        }
+    }
+}
